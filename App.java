aeb868f2d59fe0bb0fc5aee4244ff40803b50d21
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-06 10:58:55 +0200

    Methode Referenties

---
+++ App.java
@@ -0,0 +1,29 @@
+package be.intecbrussel.lambda.methodeReferenties;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        // Static Method Reference
+        List<Integer> numbers = Arrays.asList(1, 4, 5);
+
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            System.out.println(numbers.get(i));
+        }
+
+        System.out.println();
+
+        for (Integer i : numbers)
+        {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        numbers.forEach(System.out::println);
+    }
+}
